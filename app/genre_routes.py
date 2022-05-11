41a83772c98064edf84ed3791a805e553a185943
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-05-11 11:13:35 -0500

    Added POST route to /genres

--- app/genre_routes.py
+++ app/genre_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models.genre import Genre
+from app import db
 
 genre_bp = Blueprint("genres", __name__, url_prefix = "/genres")
 
@@ -13,4 +14,11 @@ def get_all_genres():
 
 @genre_bp.route("", methods = ["POST"])
 def make_new_genre():
-    pass
+    request_body = request.get_json()
+
+    new_genre = Genre(name = request_body["name"])
+
+    db.session.add(new_genre)
+    db.session.commit()
+
+    return jsonify({"name": new_genre.name}), 200
